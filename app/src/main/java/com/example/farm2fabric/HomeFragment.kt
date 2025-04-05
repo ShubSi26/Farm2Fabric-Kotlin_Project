a650b17142c7404068964ac1715a8a32a7113606
shubhamgit <shubhamsi160@gmail.com>
2025-04-06 00:16:13 +0530

    created order orderNow Payments

--- app/src/main/java/com/example/farm2fabric/HomeFragment.kt
+++ app/src/main/java/com/example/farm2fabric/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.example.farm2fabric
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.constraintlayout.motion.widget.MotionLayout
+import com.example.farm2fabric.MainActivity
 
 class HomeFragment : Fragment() {
 
@@ -23,10 +26,27 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+
         val motionLayout = view.findViewById<MotionLayout>(R.id.dashboard)
         Handler().postDelayed({
             motionLayout.transitionToEnd()
         }, 700)
+
+        view.findViewById<CardView>(R.id.order_button).setOnClickListener{
+            val intent = Intent(requireContext(), Order::class.java)
+            startActivity(intent)
+        }
+
+        view.findViewById<CardView>(R.id.order_now_button).setOnClickListener{
+            val intent = Intent(requireContext(), OrderNow::class.java)
+            startActivity(intent)
+        }
+
+        view.findViewById<CardView>(R.id.payment_button).setOnClickListener{
+            val intent = Intent(requireContext(), Payments::class.java)
+            startActivity(intent)
+        }
+
         return view
     }
 
